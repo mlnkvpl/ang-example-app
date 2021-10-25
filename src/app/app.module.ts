8b5423f8f59776fc5093154a9f0b3d23bbd16c4c
Paul Melnikov <paul.melnikov@edoc.de>
2021-10-25 17:00:02 +0200

    added bootstrap and jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; // not sure about that
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { DatePipe } from '@angular/common';
+
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: 'first', component:  AppComponent},
+  { path: '',   redirectTo: '/tutorial', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
